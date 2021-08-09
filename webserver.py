c4d5e498db1d3291603a7a40234b5e9db1b5d057
pnvnd <peter@datacrunch.ca>
2021-08-09 19:40:09 -0400

    Restructured code to remove credentials

--- webserver.py
+++ webserver.py
@@ -1,9 +1,35 @@
+# pip install snowflake-connector-python
+# pip install snowflake-sqlalchemy
+
+
 from flask import Flask
+from snowflake import connector
 
 app = Flask("My Website")
 
 @app.route("/")
-def homepage():
-    return "Weclome to my website!"
+def home():
+    return "Weclome to my website!  My snowflake account # is: " + str(onerow)
+
+@app.route("/about")
+def about():
+    return "About my website!"
+
+# Credentials
+file = open("credentials", "r")
+credentials = json.load(file)
+file.close()
+
+
+# Snowflake
+cnx = connector.connect(
+    account = "ci21584.ca-central-1.aws",
+    user = "datacrunch",
+    password = "Enterprise1!"
+)
+
+cur = cnx.cursor()
+cur.execute("select current_account()")
+onerow = cur.fetchone()
 
 app.run()
